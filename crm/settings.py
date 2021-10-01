d0fa2a64770685208434010efa94b0a2faccda76
Vicolas11 <incrediblechamp1@gmail.com>
2021-10-01 10:07:24 +0100

    Modified Settings

--- crm/settings.py
+++ crm/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
             'NAME': env('DB_NAME'), 
             'USER': env('DB_USER'), 
             'PASSWORD': env('DB_PWD'),
-            'HOST': 'ec2-3-218-47-9.compute-1.amazonaws.com', 
+            'HOST': 'ec2-44-198-204-136.compute-1.amazonaws.com', 
             'PORT': '5432',
         }
 }
